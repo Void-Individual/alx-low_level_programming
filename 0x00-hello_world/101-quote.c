d6fb5294527b483ad0ce777347faeebd33f3bfbb
Void-Individual <otayomitv@gmail.com>
2023-09-07 13:27:49 -0700

    yes!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+*main - the function used to initialize output
+*
+*Return: 1 (Requirement for this program)
+*
+*/
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
