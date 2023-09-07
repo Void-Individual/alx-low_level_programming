539384822d3a4671570dc2a1f4cc3fbc88a1bdbf
Void-Individual <otayomitv@gmail.com>
2023-09-07 11:00:16 -0700

    again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(void)
+/*string begins*/
 {
 /*string begins*/
 char string[] = "\"Programming is like building a multilingual puzzle";
 
 puts(string);
 
-return(0);
+return (0);
 }
