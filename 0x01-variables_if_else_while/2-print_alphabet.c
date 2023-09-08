ccff4477149ccdd404e665b29382f43f0d9740ef
Void-Individual <otayomitv@gmail.com>
2023-09-08 12:05:42 -0700

    removed last digit warning fixed alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-/*declare alpha as a to begin the range a - z*/
+/*declare variable a to begin the range a - z*/
 char alpha = 'a';
 /*print the value of alpha if its z*/
 while (alpha <= z)
