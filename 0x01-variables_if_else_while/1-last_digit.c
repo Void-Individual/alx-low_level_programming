acbee9d44c6ce2a45bb3b8b21cc49d29683f3315
Void-Individual <otayomitv@gmail.com>
2023-09-08 09:06:11 -0700

    First try at last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,22 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
+int l = n % 10;
+/**
+*This process divides the random value of n by 10
+*
+*and leaves the last digit as the remainder
+*/
+if (l > 5)
+{printf("Last digit of %d is %d and is greater than 5\n", n, l);
+}
+else if (l == 0)
+{printf("Last digit of %d is %d and is 0\n", n, l);
+}
+else if (l < 6, l != 0)
+/*This rule means l is less than 6 but not equal to 0*/
+{printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+}
 
 return (0);
 }
