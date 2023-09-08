4e66f17f4c390b65bf9eba10a6a689d11fdc5437
Void-Individual <otayomitv@gmail.com>
2023-09-08 13:29:38 -0700

    fixed the infinite loop in numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,14 +8,14 @@ int main(void)
 /*declare a variable to contain tha range 0 - 9*/
 int num = 0;
 /*compare with the last variable*/
-while (num <= '9')
+while (num <= 9)
 {
 /*print output*/
 printf("%d", num);
 /*increment by 1*/
 num++;
 /*set a condition for when the final value is reached*/
-if (num == '9')
+if (num == 9)
 /*print a newline*/
 printf("\n");
 }
