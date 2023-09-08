8579dab4a8460e1452e21422fbabd479da69db0a
Void-Individual <otayomitv@gmail.com>
2023-09-08 15:37:14 -0700

    first try at base 16 and reversed alphabets

---
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - initiates code
+*Return: 0 (Success)
+*/
+int main(void)
+{
+/*set variable for z to cover range z - a*/
+char alpha = z;
+/*set a conditionto compare its values*/
+while (alpha >= a)
+{
+/*print the output character*/
+putchar(alpha);
+/*decrease value by 1*/
+alpha--;
+}
+/*print a newline*/
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - executes the code
+*Return: 0 (For Success)
+*/
+int main(void)
+{
+/*declare variables for the hexadecimal numbers*/
+int num = 0;
+/*declare variables for the hexadecimal letters*/
+char alpha = a;
+/*give the conditions for output of hexa numbers*/
+while (num <= 9)
+{
+/*print output value*/
+putchar(48 + num);
+/*increment value by 1*/
+num++;
+}
+/*give condition for output of hexa letters*/
+while (alpha <= f)
+{
+/*print output value*/
+putchar(alpha);
+/*increment by 1*/
+alpha++;
+}
+/*print a newline*/
+putchar('\n');
+return (0);
+}
