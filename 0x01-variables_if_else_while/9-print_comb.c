4aa0415739d257a5422dd0fe667311dd32027695
Void-Individual <otayomitv@gmail.com>
2023-09-08 16:53:43 -0700

    3rd try at possible combo task 1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,17 @@ int main(void)
 /*set variable for the value*/
 int num = 0;
 /*set the conditions*/
-while (num < 10)
+while (num <= 9)
 {
 /*print output*/
 putchar(num + '0');
+if (num != 9)
+{
 /*print comma*/
 putchar(',');
 /*print space*/
 putchar(' ');
+}
 /*add increment*/
 num++;
 }
