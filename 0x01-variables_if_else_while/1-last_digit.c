d306868a8b986b6523d33d4b47aa65385dcfcf0b
Void-Individual <otayomitv@gmail.com>
2023-09-08 08:38:58 -0700

    gitignore emacs

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - initializes the program
+*
+*Return: 0 (This programs success)
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+
+return (0);
+}
