3a9abf4dd607fa105beb06055e7c9491bf126f28
Void-Individual <otayomitv@gmail.com>
2023-09-08 16:41:08 -0700

    possible combo task 1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - a program to print all possible combo of single digit numbers
+*Return: 0 (success)
+*/
+int main(void)
+{
+/*set variable for the value*/
+int num = 0;
+/*set the conditions*/
+while (num < '10')
+{
+/*print output*/
+putchar(num + '0');
+/*print comma with space*/
+putchar(', ');
+/*print space*/
+putchar(' ');
+/*add increment*/
+num++;
+}
+putchar('\');
+return (0);
+}
