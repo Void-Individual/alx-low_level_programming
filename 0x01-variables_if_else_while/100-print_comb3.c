5028d62a7ebd28f2d4d1873321944d3318cf81e2
Void-Individual <otayomitv@gmail.com>
2023-09-09 07:12:59 -0700

    fixed code: changed int to ascii text for putchar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,25 @@
 int main(void)
 {
 /*set variable for the first and second digits*/
-int ten = '0';
+int ten = 0;
 int unit;
 /*to ensure that unit will never be equal to ten*/
-unit = ten + '1';
+unit = ten + 1;
 /*set condition for ten is less than 9*/
-while (ten < '9')
+while (ten < 9)
 {
 /*set condition for unit isn't more than 9*/
-if (unit <= '9')
+if (unit <= 9)
 {
 /*to ensure there be no coincidental repetition*/
 if (ten != unit)
 {
 /*print the ten*/
-putchar(ten);
+putchar(ten + 48);
 /*print the unit*/
-putchar(unit);
+putchar(unit + 48);
 /*set condition to print period until the last combo*/
-if (ten != '8' && unit != '9')
+if (ten != 8 && unit != 9)
 {
 putchar(',');
 putchar(' ');
