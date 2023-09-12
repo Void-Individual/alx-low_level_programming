dc80cb74ec89b04c43315cf1991c62e9992e2322
Void-Individual <otayomitv@gmail.com>
2023-09-12 08:52:00 -0700

    task 1 and 2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+/**
+*main - program to print _putchar followed by a newline
+*Return: 0 (Success)
+*/
+int main(void)
+{
+char i = putchar;
+putchar('_');
+putchar('i');
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <main.h>
+/**
+*main - print the alphabet in lowercase, with a newline
+*Return: 0 (Success)
+*/
+int main(void)
+{
+/*set starting variable a */
+char alpha = 'a';
+/*set control range*/
+while (alpha <= 'z')
+{
+/*print the letter*/
+putchar(alpha);
+/*increment the value by 1*/
+alpha++;
+}
+/*print the newline seprately*/
+putchar('\n');
+return (0);
+}
