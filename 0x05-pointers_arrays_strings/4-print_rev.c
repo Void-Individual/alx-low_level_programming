31431350674ccefabcda1de59b563858757acfe3
Void-Individual <otayomitv@gmail.com>
2023-09-18 15:38:03 -0700

    used double while loops since string length is unknown

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: pointer variable
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (x >= 0)
+	{
+		if (s[x] == '\0')
+			break;
+		x++;
+	}
+
+	while (x >= 0)
+	{
+		_putchar(s[x]);
+		x--;
+	}
+
+	_putchar('\n');
+}
