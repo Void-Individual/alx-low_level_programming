c53cc78682dd1da5d2af57520206768cede7f1fe
Void-Individual <otayomitv@gmail.com>
2023-09-18 17:48:01 -0700

    almost got this wrong

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts -  a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: variable of the pointer
+ */
+
+void puts2(char *str)
+{
+	int x = 0;
+
+	while (x >= 0)
+	{
+		if (str[x] == '\0')
+			break;
+		_putchar(str[x]);
+		x = x + 2;
+	}
+
+	_putchar('\n');
+}
