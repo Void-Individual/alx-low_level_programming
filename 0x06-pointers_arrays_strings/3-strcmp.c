2578b78815ced448bcdd1a5fa2daacedee1a5800
Void-Individual <otayomitv@gmail.com>
2023-09-21 12:53:52 -0700

    it worked?

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: string to be compared
+ * @s2: string to be compared
+ * Return: the value of the comparison
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int y = 0;
+	int x = 0;
+	int n = 0;
+	char a = *(s1 + x);
+	char b = *(s2 + y);
+
+		n = n +(a - b);
+
+	return (n);
+}
