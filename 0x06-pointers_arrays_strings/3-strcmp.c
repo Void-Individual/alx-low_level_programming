c22fec2e34182d5bca1f39e8e425da5891b27db6
Void-Individual <otayomitv@gmail.com>
2023-09-22 02:28:31 -0700

    increased character range of the var n

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,23 @@ int _strcmp(char *s1, char *s2)
 	int y = 0;
 	int x = 0;
 	int n = 0;
-	char a = *(s1 + x);
-	char b = *(s2 + y);
+	char a = 0;
+	char b = 0;
 
+	for (n = 0; n >= 0; x++, y++)
+	{
+		a = *(s1 + x);
+		b = *(s2 + y);
+
+		if (*(s1 + x) == '\0')
+			break;
+		if (*(s2 + y) == '\0')
+			break;
 		n = a - b;
+
+		if (n != 0)
+			break;
+	}
 
 	return (n);
 }
