7e0c8b2346db464ebb3d20ec1668915b44087790
Void-Individual <otayomitv@gmail.com>
2023-09-28 13:59:29 -0700

    please work

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,5 @@
 #include "main.h"
 /**
- * is_prime_number -  a function that returns 1 if the
- * input integer is a prime number, otherwise return 0
- * @n: input variable
- * Return: 1 if prime, otherwise 0
- */
-
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-	return (_cp_test(n, n / 2));
-}
-
-/**
  * _cp_test - test for prime numbers
  * @n: number to be tested
  * @a: test numbers
@@ -25,6 +11,20 @@ int _cp_test(int n, int a)
 	if (a == 1)
 		return (1);
 	if ((n % a) == 0)
-		return (0);
+ 		return (0);
 	return (_cp_test(n, a - 1));
+}
+
+/**
+ * is_prime_number -  a function that returns 1 if the
+ * input integer is a prime number, otherwise return 0
+ * @n: input variable
+ * Return: 1 if prime, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (_cp_test(n, n / 2));
 }
