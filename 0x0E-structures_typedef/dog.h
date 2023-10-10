c1fc0b3e1b23c244b66ffcb56d54619605d41276
Void-Individual <otayomitv@gmail.com>
2023-10-10 13:56:42 -0700

    updated header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores info of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 
 struct dog
 {
@@ -8,5 +20,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
