dc566cd89559571a66b4a5838682f0f723b4224b
Void-Individual <otayomitv@gmail.com>
2023-10-20 21:02:19 -0700

    fixed

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,12 @@
 section .data
-    hello db "Hello, Holberton",0  ; The string to print, null-terminated
+	hello db "Hello, Holberton",0  ; The string to print, null-terminated
 
 section .text
-    global main
+	global main
 
 extern printf
 
 main:
-    mov rdi, hello         ; The format string for printf
-    call printf            ; Call printf
-    ret                    ; Exit the program
+	mov rdi, hello         ; The format string for printf
+	call printf            ; This calls printf
+	ret                    ; Exit the program
