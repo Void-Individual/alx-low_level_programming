8e2a99eb2490e26cccf02f4fcd40eadff286f7c4
Void-Individual <otayomitv@gmail.com>
2023-10-25 01:26:29 +0100

    omo

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,7 @@ size_t print_listint(const listint_t *h)
 	const listint_t *temp;
 
 	if (h == NULL)
-	{
-                printf("(nil)\n");
-                return (count);
-        }
+	        return (count);
 
 	temp = h;
 	while (temp != NULL)
