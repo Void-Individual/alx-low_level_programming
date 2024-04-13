57a658b3567b687c0ca51a9b776b2b593a5d5ecf
Void-Individual <otayomitv@gmail.com>
2024-04-13 22:14:55 +0100

    ugh

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,28 +1,6 @@
 #include "search_algos.h"
 
 /**
- * print_array - Function to print the array
- * @array: array to be printed
- * @min: minimum index
- * @max: maximum index
- * Return: empty
-*/
-
-void print_array(int *array, size_t min, size_t max)
-{
-	printf("Searching in array: ");
-	while (min < max)
-	{
-		printf("%d", array[min]);
-		min++;
-		if (min == max)
-			printf("\n");
-		else
-			printf(", ");
-	}
-}
-
-/**
  * binary_search - A function that searches for a value in a sorted
  * array using the Binary search algorithm
  * @array: pointer to the first element in the array
@@ -33,25 +11,29 @@ void print_array(int *array, size_t min, size_t max)
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t count = 0, half = size / 2;
-	int result;
+	size_t left = 0, right = size - 1, mid = 0, index;
 
-	if ((!array) | (size == 0))
+	if (!array || size == 0)
 		return (-1);
 
-	print_array(array, count, size);
-
-	if (value == array[half])
-		return ((int)half);
-	else if (value < array[half])
-		return(binary_search(array, half, value));
-	else
+	while (left <= right)
 	{
-		result = binary_search(array + half + 1, size - half - 1, value);
-        if (result == -1)
-			return (-1);
+		printf("Searching in array: ");
+		for (index = left; index <= right; index++)
+		{
+			printf("%d", array[index]);
+			if (index < right)
+				printf(", ");
+		}
+		printf("\n");
+		mid = (left + right) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
 		else
-			return (half + 1 + result);
+			right = mid - 1;
 	}
 
 	return (-1);
